3a79cbfc6b11bc61446ec6acc75665f9e304fdb8
wilhempierre <wilhem@ymail.com>
2014-05-17 01:30:04 +0900

    Initial commit

---
+++ numbers.rb
@@ -0,0 +1,36 @@
+# prints integer to screen
+  puts 1+2
+
+# to print my name as a string
+  puts "Hi, I'm wil"
+
+# to print floating numbers on screen
+  puts 1.0 + 2.0
+  puts 2.0 * 3.0
+  puts 5.0 - 8.0
+  puts 9.0 / 2.0
+
+# puts numbers in order of operation
+  puts 5 * (12-8) + -15
+  puts 98 + (59872 / (13*8)) * -51
+
+# first problem to solve
+# hours in a year?
+# year = 365 days
+# day = 24 hours
+  hours_in_a_year = 365 * 24
+  puts hours_in_a_year
+
+# Minutes in a decade?
+  minutes_in_decade = (2 + 10 * 365) * 24 * 60
+  puts minutes_in_decade
+
+# it all depends on leap year
+  minutes_in_decade = (3 + 10 * 365) * 24 * 60
+  puts minutes_in_decade
+
+# my age in seconds
+  puts 60 * 60 * 24 * (365 * 34 + 9)
+
+# my sister is 1025000000 seconds old, change to years.
+  puts 1025000000/(60*60*24*365)
