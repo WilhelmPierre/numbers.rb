9ddf71d91fb992a7c9132894f9d16942f9be6b12
wilhempierre <wilhem@ymail.com>
2014-05-17 10:49:48 +0900

    Showing variable assignment in Ruby

---
+++ Variables.rb
@@ -0,0 +1,17 @@
+# Variable assignment
+  my_string = '...you can say that again...'
+  puts my_string
+
+# Another variable
+  name = 'Wil'
+  puts 'my name is ' + name +  '.'
+
+# Just one more exercise about variables
+  composer = 'Mozart'
+  puts composer + "was the best in his time"
+
+# A few more variables exercises
+  var1 = 8
+  var2 = var1
+  puts var1
+  puts var2
