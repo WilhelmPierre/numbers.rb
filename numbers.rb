8a24154cc0ea93f3db12596fd2a5d65ed914b5a4
wilhempierre <wilhem@ymail.com>
2014-05-17 10:29:11 +0900

    How to use strings in Ruby

--- numbers.rb
+++ numbers.rb
@@ -34,3 +34,36 @@
 
 # my sister is 1025000000 seconds old, change to years.
   puts 1025000000/(60*60*24*365)
+
+
+# print string on screen, also empty string is possible
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# We can also add strings
+puts 'I like' + 'apple pie.'
+
+# Strings can also be multiplied
+puts 'dog ' * 4
+
+# Three examples showing difference between digits and numbers
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+# The following examples are also possible
+puts 2 * 5
+puts'2'* 5
+puts'2 * 5'
+
+# Cannot add or multiply string with non-string(errors)
+￼ puts '12' + 12
+puts '2' * '5'
+puts 'Fred' * 'John'
+puts 'Betty' + 12
+
+# Showing how to use an escape character in a string
+puts 'You\'re smart'
+# Solve by using " " double quotes
+" I'm smart"
